c28e3ff94054ba67da6765e2c3bb8c36f286a4e5
kael <kael_yp@foxmail.com>
2020-04-12 21:29:43 +0800

    arrange: 发放01 02 03

--- answer/kael/leetcode-02/answer.js
+++ answer/kael/leetcode-02/answer.js
@@ -7,4 +7,5 @@ var lengthOfLastWord = function(s) {
 };
 
 // 解题思路
+// isEmpty 未完题目请勿删除此行
 // 我看到题目就想到应该是XXX处理，考查的是XXX知识点
--- answer/moling3650/leetcode-03/answer.js
+++ answer/moling3650/leetcode-03/answer.js
@@ -3,9 +3,9 @@
  * @return {string}
  */
 var replaceSpace = function(s) {
-    return s.replace(/\s/g, '%20');
+
 };
 
 // 解题思路
-// 这题直接调用API完成了，在更加底层的语言中，字符串大量的拼接会带来大量的资源损耗，因为string类型是不可变的，java会用StringBuilder优化字符串拼接
-// 尝试s.replace(' ', '%20')，发现只能转一个，s.replace(/\s/g, '%20')，可以转全部
+// isEmpty 未完题目请勿删除此行
+// 我看到题目就想到应该是XXX处理，考查的是XXX知识点
