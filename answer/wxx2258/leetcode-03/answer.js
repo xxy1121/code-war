3c32c3322ac13e7eba3ad6665a3edb8b337f356a
wxx2258 <872189787@qq.com>
2020-04-13 22:48:37 +0800

    answer(wxx2258):01添加贪心解法;02,03完成

--- answer/wxx2258/leetcode-03/answer.js
+++ answer/wxx2258/leetcode-03/answer.js
@@ -3,9 +3,19 @@
  * @return {string}
  */
 var replaceSpace = function(s) {
-
-};
+    let result = '';
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === ' ') {
+            result += '%20';
+        } else {
+            result += s[i];
+        }
+    }
+    return result;
+}
+// var replaceSpace = function(s) {
+//     return s.replace(/\s/g, '%20');
+// };
 
 // 解题思路
-// isEmpty 未完题目请勿删除此行
-// 我看到题目就想到应该是XXX处理，考查的是XXX知识点
+// 我看到题目就想到字符串替换，没太get到要考察的点
