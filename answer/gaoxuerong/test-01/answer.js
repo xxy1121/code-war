d209006d115559653e1916c24cb0767e36521c91
gaoxuerong <gaoxuerong123@gmail.com>
2020-04-15 22:18:33 +0800

    feat 第一周

--- answer/gaoxuerong/test-01/answer.js
+++ answer/gaoxuerong/test-01/answer.js
@@ -1,9 +1,46 @@
 /**
+ * 最大子序和
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  let res = nums[0]
+  let sum = 0
+  for (let num of nums) {
+    if (sum > 0) {
+      sum += num
+    } else {
+      sum = num
+    }
+    res = Math.max(res,sum)
+  }
+  return res
+};
+/**
+ * 最后一个单词的长度
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+  let res = 0;
+  if (s.length) {
+      let sArray = s.trim().split(' ')
+      let resString = sArray[sArray.length - 1]
+      res = resString.length
+  }
+  return res
+};
+/**
+ * 替换空格
+ */
+/**
  * @param {string} s
  * @return {string}
  */
-var reverseWords = function(s) {
+var replaceSpace = function(s) {
+  return s.split(' ').join('%20')
 };
-
-// 解题思路
-// 我看到题目就想到应该是要用array的reverse来处理，同时要注意多个空格的问题
