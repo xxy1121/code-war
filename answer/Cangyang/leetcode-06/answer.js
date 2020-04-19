7b8467fe6e1b26cc9d5fcea550c2c6d3e408f327
Cangyang <ytf25206@gmail.com>
2020-04-20 02:17:26 +0800

    answer: 解题

--- answer/Cangyang/leetcode-06/answer.js
+++ answer/Cangyang/leetcode-06/answer.js
@@ -10,8 +10,17 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
-
+	// 解题思路：
+	// 1、head 与 next互相交换
+	
+	if(head == null || head.next == null){
+           return head;
+    }
+    var next = head.next;
+    head.next = swapPairs(next.next);
+    next.next = head;
+    return next;
 };
-// 解题思路
-// isEmpty 未完题目请勿删除此行
-// 我看到题目就想到应该是XXX处理，考查的是XXX知识点
+
+// 执行用时 :60 ms, 在所有 JavaScript 提交中击败了87.31%的用户
+// 内存消耗 :33.6 MB, 在所有 JavaScript 提交中击败了100.00%的用户
