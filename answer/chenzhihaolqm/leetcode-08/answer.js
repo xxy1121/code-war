9ebd3bee82d9f1cee2937f67a4872d88afacd827
kael <kael_yp@foxmail.com>
2020-04-25 23:16:28 +0800

    arrange: 07 08 09

---
+++ answer/chenzhihaolqm/leetcode-08/answer.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+
+};
+// 解题思路
+// isEmpty 未完题目请勿删除此行
+// 我看到题目就想到应该是XXX处理，考查的是XXX知识点
