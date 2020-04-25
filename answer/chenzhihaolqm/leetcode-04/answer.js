f0ac535b5c36488f7d8502d8699beabb768193de
chenzhihaolqm <760154322@qq.com>
2020-04-25 17:41:58 +0800

    answer: 04-reverseList

--- answer/chenzhihaolqm/leetcode-04/answer.js
+++ answer/chenzhihaolqm/leetcode-04/answer.js
@@ -6,13 +6,23 @@
  * }
  */
 /**
- * @param {ListNode} head
+ * @param {ListNode} current
+   @param {ListNode} prev
  * @return {ListNode}
  */
-var reverseList = function(head) {
-
+var reverseList = function(current, prev) {
+   if(!current) {
+     return current;
+   }
+   let nextNode = current.next;
+   current.next = prev || null;
+   if(!nextNode) {
+      return current;
+   }
+   return reverseList(nextNode, current);
 };
 
+
 // 解题思路
-// isEmpty 未完题目请勿删除此行
 // 我看到题目就想到应该是XXX处理，考查的是XXX知识点
+// 想到的是递归生成链表，递归反转链表
