1088454e066e5dad89024e1d74b94fe9a02b6b32
kael <kael_yp@foxmail.com>
2020-04-25 13:41:32 +0800

    answer: 04-kael反转链表

--- answer/kael/leetcode-04/answer.js
+++ answer/kael/leetcode-04/answer.js
@@ -10,9 +10,20 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-
+    let pre = null
+    let cur = head
+    while(cur) {
+        let node = cur.next
+        cur.next = pre
+        pre = cur
+        cur = node
+    }
+    return pre
 };
 
 // 解题思路
-// isEmpty 未完题目请勿删除此行
-// 我看到题目就想到应该是XXX处理，考查的是XXX知识点
+// 这题应该就是修改node.next的指向上一个node就行了。
+// 这里只要注意在修改指针之前用一个变量存起来就可以了。
+// 本来想着是把链表的node一个个的push到一个数组里，然后反转这个数组，然后再一一修改数组的每一项的next指针，这样太蠢了。
+// 最后返回反转链表的第一项就行了，cur最终一定是原始链表的最后一项的next,所以它一定是null或undefined。
+// 这样的话，pre最后一定反转之后的第一项。
