e10b7faef247fc15d9f940d24e54801455f4f562
kael <kael_yp@foxmail.com>
2020-04-25 15:00:04 +0800

    answer: 06-kael反转链表的相邻项

--- answer/kael/leetcode-06/answer.js
+++ answer/kael/leetcode-06/answer.js
@@ -10,8 +10,24 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
-
+    if (!head) return null
+    if (!head.next) return head
+    let preNode = null // 上一项
+    let fNode = head // 交换的第一项
+    let sNode = fNode.next // 交换的第二项
+    let nextNode = sNode.next // 下一项
+    let h = fNode // 存好第一项
+    while(fNode&&sNode) { // 交换的两项都存在才执行
+          preNode && (preNode.next = sNode) // preNode存在就调整指针
+          sNode.next = fNode
+          fNode.next = nextNode
+          preNode = fNode
+          fNode = preNode.next
+          sNode = fNode.next
+          nextNode = sNode.next
+    }
+    return h
 };
 // 解题思路
-// isEmpty 未完题目请勿删除此行
-// 我看到题目就想到应该是XXX处理，考查的是XXX知识点
+// 基本思路就是两两一组，串行交换。因为要交换的是相邻的两项，那么next指针有变化是应该是交换本身的两项加上前后两项。共四项。
+// 所以，我们需要用四个变量把这四项分别存起来，再修改对应的next
