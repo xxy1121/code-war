97a25109ebd75ee1d49adeffbb1771d58bfcf6a4
mosiya <shemsy@126.com>
2020-04-27 17:35:43 +0800

    answer: 05(reverseBetween)

--- answer/mosiya/leetcode-05/answer.js
+++ answer/mosiya/leetcode-05/answer.js
@@ -12,8 +12,27 @@
  * @return {ListNode}
  */
 var reverseBetween = function(head, m, n) {
+    let pre = p = new ListNode(), start, first, count = n - m;
+    pre.next = head
 
+    for(let i = 0; i < m - 1; i++){
+        pre = pre.next
+    }
+    start = pre // start保存区间开始的前一个节点
+    first = pre.next // first保存区间开始的第一个节点
+    cur = pre.next
+    for(let i = 0; i <= count; i++) {
+        let next = cur.next
+        cur.next = pre
+        pre = cur
+        cur = next
+    }
+    start.next = pre
+    first.next = cur
+
+    return p.next
 };
 // 解题思路
-// isEmpty 未完题目请勿删除此行
-// 我看到题目就想到应该是XXX处理，考查的是XXX知识点
+// 关键点是需要存储反转的前一个节点以及反转的第一个节点，反转操作参考上一题的思路，最后需保存反转后的最后一个节点以及该节点的下一个节点。
+// 最后将反转的前一个节点指向反转结束后的最后一个节点，反转的第一个节点指向反转结束后最后一个节点的下一个节点即可
+// 这里有个需要注意的点就是，给整个链接加上一个空的头结点，会更方便整个链表的遍历操作，因为有可能从第一个节点就开始反转
